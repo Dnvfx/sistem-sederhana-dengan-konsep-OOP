bd77c09d420c7e091281b468c71a9cb80767f570
Danu Teguh Heri Prasetyo <124540466+Dnvfx@users.noreply.github.com>
2024-03-29 20:13:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UTS</title>
+</head>
+<body>
+    <h1>Selamat Datang</h1>
+    <ul>
+        <li><a href="user_management.php">User Management</a></li>
+        <li><a href="produk_management.php">Product Management</a></li>
+        <li><a href="kategori_management.php">Category Management</a></li>
+        <li><a href="keranjang_management.php">Cart Management</a></li>
+        <li><a href="order_management.php">Order Management</a></li>
+    </ul>
+</body>
+</html>
